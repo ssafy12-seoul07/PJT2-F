ace84b7d601bcfd2e3959fefed6d0a0fa6fcb5c8
newbieJanghan <bjh4135@gmail.com>
2024-08-30 14:53:48 +0900

    code clean

--- js/reviews.js
+++ js/reviews.js
@@ -1,16 +1,12 @@
 import { items } from "./videoData.js";
 
-console.log(items);
 const urlParams = new URLSearchParams(window.location.search);
 const videoId = urlParams.get("id");
 
 // Find the video in the items array
 const video = items.find((v) => v.id === videoId);
 
-console.log(videoId);
-console.log(video);
 // Set the video data into the page
-
 const titleTag = document.getElementById("video-title");
 const linkTag = document.getElementById("video-hyperlink");
 const thumbnailTag = document.getElementById("video-thumbnail");
